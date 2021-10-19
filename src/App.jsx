1e7a862ca0b66b70a65c55d6e3c2f638b80e019e
ennnm <jiaen.1sc4@gmail.com>
2021-10-19 21:41:41 +0800

    started on bill element

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import CreateBill from './components/create-bill.jsx';
+import FormElem from './components/form.jsx';
+import BillElem from './components/bill-elem.jsx';
 
 export default function App() {
   const [isBillCreated, setBillCreated] = useState(false);
   const [bill, setBill] = useState();
+
+  const [itemList, setItemList] = useState([]);
+  const [peopleList, setPeopleList] = useState([]);
+
+  console.log('itemList :>> ', itemList);
+  console.log('peopleList :>> ', peopleList);
+  const addToItems = (item) => {
+    setItemList([...itemList, item]);
+  };
+  const addToPeople = (person) => {
+    setPeopleList([...peopleList, person]);
+  };
 
   const addBill = (name) => {
     axios.put('/bill/create', { name })
@@ -21,7 +35,7 @@ export default function App() {
   return (
     <div>
       {!isBillCreated && <CreateBill addItem={addBill} />}
-      {isBillCreated && <FormElem />}
+      {isBillCreated && <FormElem addToItems={addToItems} addToPeople={addToPeople} />}
       {isBillCreated && <BillElem />}
 
     </div>
